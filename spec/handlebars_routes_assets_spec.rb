fa51deda0b3b662a0db55f1b84d0d1e0f60730b8
Raphael Randschau <nicolai86@me.com>
2012-01-30 19:35:00 +0100

    fix specs

--- spec/handlebars_routes_assets_spec.rb
+++ spec/handlebars_routes_assets_spec.rb
@@ -1,30 +1,41 @@
 require 'spec_helper'
 
+def build_route name, path
+  OpenStruct.new({:name => name, :path => OpenStruct.new(:ast => path)})
+end
+
 describe HandlebarsRoutesAssets do
   # the @route property basically describes a RouteSet#routes object
   # as Rails.application.routes.routes[:index] returns it
   describe 'resources#index' do
-    before { @route = OpenStruct.new({:name => 'users', :path => '/users(.:format)'}) }
+    before { @route = build_route 'users', '/users(.:format)' }
     subject { HandlebarsRoutesAssets::RoutesHelper.new [@route] }
 
     it { should respond_to(:users_path) }
-    its(:users_path) { should eql('/users') }
+    its(:users_path) { should eql '/users'}
   end
 
   describe 'resources#show' do
-    before { @route = OpenStruct.new({:name => 'user', :path => '/users/:id(.:format)'}) }
+    before { @route = build_route 'user', '/users/:id(.:format)' }
     subject { HandlebarsRoutesAssets::RoutesHelper.new [@route] }
 
     it { should respond_to(:user_path) }
     its(:user_path) { should eql('/users/{{id}}') }
-    it { subject.user_path('{{bar}}').should eql('/users/{{bar}}') }
+    it { subject.user_path.should eql('/users/{{id}}') }
   end
 
   describe 'resources#edit' do
-    before { @route = OpenStruct.new({:name => 'edit_user', :path => '/users/:id/edit(.:format)'}) }
+    before { @route = build_route 'edit_user', '/users/:id/edit(.:format)' }
     subject { HandlebarsRoutesAssets::RoutesHelper.new [@route] }
 
     it { should respond_to(:edit_user_path) }
     its(:edit_user_path) { should eql('/users/{{id}}/edit') }
+  end
+  
+  describe 'resources without name' do
+    before { @route = build_route nil, '/foo' }
+    subject { HandlebarsRoutesAssets::RoutesHelper.new [@route] }
+    
+    it { subject.should_not_receive(:build_url) }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ ENV["RAILS_ENV"] = "test"
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'handlebars_routes_assets/handlebars_routes_helper'
+require 'handlebars_routes_assets/routes_helper'
 require 'rspec'
 require 'ostruct'
