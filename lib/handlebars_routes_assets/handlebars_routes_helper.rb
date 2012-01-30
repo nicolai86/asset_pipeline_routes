91a18ef99b026d38b3e64da9efa54bac947578bf
Raphael Randschau <nicolai86@me.com>
2012-01-30 16:33:59 +0100

    Rails 3.2.1 compatibility

--- lib/handlebars_routes_assets/handlebars_routes_helper.rb
+++ lib/handlebars_routes_assets/handlebars_routes_helper.rb
@@ -3,6 +3,8 @@ module HandlebarsRoutesAssets
     def initialize(routes)
       default_block = '{{\1}}'
       routes.each do |route|
+        next if route.name.nil? # only handle named_routes
+        
         self.class.instance_eval do
           define_method :"#{route.name}_path" do |mapping = default_block |
             build_url route, mapping
@@ -12,7 +14,7 @@ module HandlebarsRoutesAssets
     end
 
     def build_url route, mapping
-      route.path.ast.gsub(/\(\.:\w+\)/,'').gsub(/:(\w+)/, mapping).to_s
+      route.path.ast.to_s.gsub(/\(\.:\w+\)/,'').gsub(/:(\w+)/, mapping).to_s
     end
   end
 end
