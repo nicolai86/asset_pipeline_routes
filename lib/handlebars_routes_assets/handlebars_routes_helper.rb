6ef23b6dd3d33dd16bf6f228421959d1c330067a
Raphael Randschau <nicolai86@me.com>
2012-01-30 16:48:36 +0100

    Update lib/handlebars_routes_assets/handlebars_routes_helper.rb

--- lib/handlebars_routes_assets/handlebars_routes_helper.rb
+++ lib/handlebars_routes_assets/handlebars_routes_helper.rb
@@ -1,13 +1,15 @@
 module HandlebarsRoutesAssets
   class RoutesHelper
-    def initialize(routes)
-      default_block = '{{\1}}'
+    def initialize(routes, default_block = '{{\1}}')
       routes.each do |route|
         next if route.name.nil? # only handle named_routes
         
         self.class.instance_eval do
           define_method :"#{route.name}_path" do |mapping = default_block |
             build_url route, mapping
+          end
+          define_method :"#{route.name}_gsub_path" do |id_replacement|
+            proc { |route, mapping| build_url route, mapping }.curry[route].call id_replacement
           end
         end
       end
