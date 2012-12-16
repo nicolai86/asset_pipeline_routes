4b82d9bd802280f6c160a7250378fcd2b9254258
Raphael Randschau <nicolai86@me.com>
2012-12-16 14:06:10 +0100

    add coverage generation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
-ENV["RAILS_ENV"] = "test"
+ENV["RAILS_ENV"] ||= 'test'
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails' do
+   add_filter "/.bundle/"
+end
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
